d4b76e952459e22ed1313d7364168a070f08fcb3
Daniel Savescu <71008399+daniel-savescu@users.noreply.github.com>
2023-11-06 16:24:32 +0200

    Add files via upload

--- The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
@@ -15,6 +15,8 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//Variable name conventions
+
 let daniel_matilda = "DM";
 let $function = 8;
 let PI = 3.1415;
@@ -37,6 +39,44 @@ let population = 19120000;
 
 console.log(country);
 console.log(continet);
+console.log(population);
+
+*/
+
+/* Data Types
+
+let javaScriptIsFun = true;
+
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 26);
+console.log(typeof "John");
+
+javaScriptIsFun = "YES!";
+
+console.log(typeof javaScriptIsFun);
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+age = 30;
+console.log(age);
+console.log(typeof age);
+
+console.log(typeof null);
+
+//Data Type Asignment
+
+let isIsland = false;
+let language;
+let country = "Romania";
+let population = 19120000;
+
+console.log(isIsland);
+console.log(language);
+console.log(country);
 console.log(population);
 
 */
