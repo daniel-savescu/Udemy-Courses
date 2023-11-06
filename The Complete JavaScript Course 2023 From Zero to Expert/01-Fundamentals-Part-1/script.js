2289b01000115303fc02d1e0e94dfada493fd54f
Daniel Savescu <71008399+daniel-savescu@users.noreply.github.com>
2023-11-06 14:35:59 +0200

    Values and Variables final commit

--- The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
@@ -1,3 +1,30 @@
+/* Linkin the JS file 
+
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN");
+
+*/
+
+/* Values and Variable 
+
 console.log(40 + 8 + 23 - 10);
+console.log("Daniel");
+console.log(23);
+let firstName = "Daniel";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let daniel_matilda = "DM";
+let $function = 8;
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+*/
