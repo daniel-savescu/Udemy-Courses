4b22a8cba23689c76392eeac59e2ed1f307fb7b9
Daniel Savescu <71008399+daniel-savescu@users.noreply.github.com>
2023-11-07 16:42:08 +0200

    falsy and thrutly values

--- The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/01-Fundamentals-Part-1/script.js
@@ -305,3 +305,33 @@ console.log("123" < 57);
 console.log(5 + 6 + "4" + 9 - 4 - 2);
 
 */
+
+/* Falsty and Truthly Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+console.log(Boolean(1));
+console.log(Boolean("Dan"));
+console.log(Boolean({}));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job");
+}
+
+let height = 0;
+if (height) {
+  console.log("Yey! The variable it's defined");
+} else {
+  console.log("The variable is undefined");
+}
+
+*/
