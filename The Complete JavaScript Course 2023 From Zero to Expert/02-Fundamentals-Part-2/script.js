f4b0bbdc18803411374b39e4b5bdc309203cfddd
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-10 12:58:50 +0200

    functions and assigment

--- The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/script.js
@@ -15,3 +15,37 @@ if (hasDriversLicense) console.log("I can drive !");
 // const private = 1;
 
 */
+
+/*Functions 
+
+function message() {
+  console.log("My name is Daniel");
+}
+
+//Calling, running, invoking function
+message();
+message();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//Function assignment
+
+function describeCountry(country, population, cityCapital) {
+  return `${country} has ${population} million people and its capital city is ${cityCapital}.`;
+}
+
+const finland = describeCountry("Finland", 6, "Helsinki");
+console.log(finland);
+
+*/
