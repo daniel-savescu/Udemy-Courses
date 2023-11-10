ac6298dc9e5d20fe01cac3becdc7baf2a1ab6589
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-10 13:20:57 +0200

    function declaration and expressions + assignment

--- The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/script.js
@@ -49,3 +49,39 @@ const finland = describeCountry("Finland", 6, "Helsinki");
 console.log(finland);
 
 */
+
+/* Function declaration / expressions ( anonymous functions )
+
+//Function declaration
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
+}
+
+let myAge = calcAge1(1990);
+console.log(myAge);
+
+//Function expression (Anonymous Function)
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+
+const age = calcAge2(1963);
+console.log(age);
+
+//Function declaration / expressions assigment
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const Romania = percentageOfWorld1(19);
+console.log(Romania);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const China = percentageOfWorld2(1441);
+console.log(China);
+
+*/
