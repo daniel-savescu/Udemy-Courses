ebe20a62b522d44e38e97432318dd83aef46e54c
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-10 14:57:40 +0200

    challenge_01

---
+++ The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/challenge_01.js
@@ -0,0 +1,18 @@
+/* Write your code below. Good luck! 🙂 */
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
