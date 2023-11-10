e823472ed89e880b80665e3bd41fb140099d65dd
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-10 12:36:21 +0200

    Strict mode

---
+++ The Complete JavaScript Course 2023 From Zero to Expert/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* Strict mode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicense = true;
+}
+
+if (hasDriversLicense) console.log("I can drive !");
+
+// const interface = "Audio";
+// const private = 1;
+
+*/
