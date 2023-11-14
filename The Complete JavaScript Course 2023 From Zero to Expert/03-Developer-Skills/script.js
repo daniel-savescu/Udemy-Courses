28492843951233e11928751bdafd90e9ee14286b
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-14 13:23:22 +0200

    developer skills section

---
+++ The Complete JavaScript Course 2023 From Zero to Expert/03-Developer-Skills/script.js
@@ -0,0 +1,11 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+/*Configure prettier
+
+const x = 23;
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2023 - birthYear;
+
+*/
