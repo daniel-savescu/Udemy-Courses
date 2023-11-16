87e422f7aa6bbdb91c837a3aec138efd3d5eb8dd
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-16 13:49:44 +0200

    Selecting and manipulating elements

--- The Complete JavaScript Course 2023 From Zero to Expert/03-Developer-Skills/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/03-Developer-Skills/script.js
@@ -1,11 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-/*Configure prettier
-
 const x = 23;
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2023 - birthYear;
 
-*/
+console.log(calcAge(1991));
