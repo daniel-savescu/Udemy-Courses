2300f8b175b5e2237a3c76d1c3f54275e5a162ff
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-22 15:22:47 +0200

    rest pattern and parameters

--- The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -41,8 +41,51 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngrediend, ...otherIngredients) {
+    console.log(mainIngrediend);
+    console.log(otherIngredients);
+  },
 };
 
+/* Rest Pattern and Parameters
+
+//DESTRUCTURING
+
+//SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST , because of LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//FUNCTIONS
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+*/
 /*The spread Operator
 
 const arr = [7, 8, 9];
