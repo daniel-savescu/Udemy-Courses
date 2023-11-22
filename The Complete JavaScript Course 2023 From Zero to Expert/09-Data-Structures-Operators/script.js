4575b22baa9975fd0f60e81284a6bce4978beb5b
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-22 14:26:49 +0200

    Spread Operator

--- The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -36,8 +36,59 @@ const restaurant = {
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
+/*The spread Operator
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join multiple arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Daniel';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+
+//Real world example
+// const ingredients = [
+//   prompt("Let's make pasta ! Ingredinet 1?"),
+//   prompt('Ingrediend 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Rome Restaurant';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+*/
+
+/* Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
