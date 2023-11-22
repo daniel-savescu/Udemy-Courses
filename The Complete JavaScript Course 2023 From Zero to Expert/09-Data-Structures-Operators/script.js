ad6dbc19c05a6e67c87ba8413c4a692229fbdf16
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-22 16:30:14 +0200

    short circuiting OR and AND

--- The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -47,6 +47,37 @@ const restaurant = {
   },
 };
 
+/*Short circuiting || and  && 
+
+//OR OPERATOR
+// Use ANY data type , return ANY data type,
+//short-circuiting
+console.log(3 || 'Daniel');
+console.log('' || 'Daniel');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+//AND OPERATOR
+
+console.log(0 && 'Daniel');
+console.log(7 && 'Daniel');
+console.log('Hello' && 23 && null && 'Daniel');
+
+//Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'olives');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+*/
 /* Rest Pattern and Parameters
 
 //DESTRUCTURING
