9adb63e9a2196c452530f61485db8a0f7911fa63
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-11-23 11:52:22 +0200

    The Coalescing Operator

--- The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -47,6 +47,18 @@ const restaurant = {
   },
 };
 
+/* The Nullish Coalescing Operator 
+restaurant.numGuests = 0;
+
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+//Nullish: null and undefined ( NOT 0 or '' )
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+
+*/
+
 /*Short circuiting || and  && 
 
 //OR OPERATOR
