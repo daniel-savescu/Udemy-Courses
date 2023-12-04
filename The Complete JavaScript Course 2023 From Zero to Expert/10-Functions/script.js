ce3e0ffd087455ad17d4f9f53f1fc7e0766de1e8
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-04 12:13:10 +0200

    Functions passing as : value vs reference

--- The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
@@ -26,3 +26,32 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
+
+/* Functions passing as : value vs reference
+const flight = 'LH234';
+const daniel = {
+  name: 'Daniel Savescu',
+  passport: 2454365464,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 2454365464) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+//checkIn(flight, daniel);
+//console.log(flight);
+//console.log(daniel);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(daniel);
+checkIn(flight, daniel);
+*/
