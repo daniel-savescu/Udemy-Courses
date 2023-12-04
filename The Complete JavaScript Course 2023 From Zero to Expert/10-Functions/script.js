294f5f250bfee4413f553f7763a27551b131b8f7
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-04 15:11:22 +0200

    Apply and Call methods

--- The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
@@ -107,3 +107,43 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 greetArr('Hi')('Daniel');
 
 */
+
+/*Apply and call methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} book a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Daniel Savescu');
+lufthansa.book(545, 'John Doe');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+//Does NOT work
+//book(23, 'Sarah William');
+
+//Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+book.call(lufthansa, 239, 'Cooper Mary');
+console.log(lufthansa);
+
+//Apply method
+const flightData = [538, 'George Cooper'];
+book.apply(eurowings, flightData);
+console.log(eurowings);
+*/
