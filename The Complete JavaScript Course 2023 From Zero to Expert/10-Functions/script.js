a184f2abfec3286f8f6c1b5e62a7766182428dd0
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-04 14:28:19 +0200

    Callback Functions

--- The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
@@ -55,3 +55,35 @@ const newPassport = function (person) {
 newPassport(daniel);
 checkIn(flight, daniel);
 */
+
+/*Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [firstWord, ...others] = str.split(' ');
+  return [firstWord.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const sayHello = function () {
+  console.log('Hello!');
+};
+
+document.body.addEventListener('click', sayHello);
+
+['Daniel', 'John', 'Sandra'].forEach(sayHello);
+
+*/
