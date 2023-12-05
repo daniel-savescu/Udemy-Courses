35aa9c3899232cc5d393cdc9588fae3fd95e757e
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-05 16:11:39 +0200

    More closure examples

--- The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/10-Functions/script.js
@@ -201,7 +201,7 @@ runOnce();
 (() => console.log('This will never run again!'))();
 */
 
-//Closure
+/*Closure
 
 const secureBooking = function () {
   let passengerCount = 0;
@@ -216,3 +216,41 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+*/
+/*More closure examples
+
+//Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+h();
+f();
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers.`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+boardPassengers(180, 3);
+*/
