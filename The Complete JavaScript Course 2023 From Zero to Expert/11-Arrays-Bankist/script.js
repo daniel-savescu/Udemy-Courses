7334a7a649c798d07fff6aef9262dfeee909e921
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-06 13:14:15 +0200

    Looping arrays : forEach

--- The Complete JavaScript Course 2023 From Zero to Expert/11-Arrays-Bankist/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/11-Arrays-Bankist/script.js
@@ -71,8 +71,6 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 /*Simple array methods
@@ -122,4 +120,24 @@ console.log(arr.at(-1));
 console.log('daniel'.at(0));
 console.log('daniel'.at(-1));
 
+*/
+
+/*Looping arrays : forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('-----FOREACH-----');
+movements.forEach(function (movement, i) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
 */
