b0eb30720e8aa725352f797f5feb8e9841db83cc
daniel-savescu <savescu.daniel.cristian@gmail.com>
2023-12-07 13:22:21 +0200

    ForEach (Maps / Sets)

--- The Complete JavaScript Course 2023 From Zero to Expert/11-Arrays-Bankist/script.js
+++ The Complete JavaScript Course 2023 From Zero to Expert/11-Arrays-Bankist/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 /////////////////////////////////////////////////
 
 /*Simple array methods
@@ -140,4 +134,26 @@ movements.forEach(function (movement, i) {
     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 });
+*/
+
+/*ForEach (Maps / Sets)
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+//Map
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
 */
